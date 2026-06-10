0a131bc9bd6561b1be754a9db918913ea8feab9e
rubinaaa657 <rubinaaa24@st.ithub.ru>
2026-06-11 00:23:31 +0300

    upd

--- server.js
+++ server.js
@@ -1,92 +1,92 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mysql = require("mysql2");
-require("dotenv").config();
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const mysql = require("mysql2");
+// require("dotenv").config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static("PUB"));
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(express.static("PUB"));
 
-// Пул для подключения к MySQL Railway
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10
-});
-
-
-// Проверка подключения
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Ошибка подключения к MySQL:", err);
-  } else {
-    console.log("MySQL подключен!");
-    connection.release();
-  }
-});
-
-// API: Получить все брони
-app.get("/bookings", (req, res) => {
-  pool.query("SELECT * FROM bookings", (err, results) => {
-    if (err) {
-      console.error("Ошибка запроса /bookings:", err);
-      return res.json([]);
-    }
-    res.json(results);
-  });
-});
-
-// API: Создать бронь
-app.post("/book", (req, res) => {
-  const { table, date, from, to, name, surname, phone, comment } = req.body;
-
-  const now = new Date();
-  const bookingDateTime = new Date(`${date}T${from}:00`);
-  if (bookingDateTime < now) {
-    return res.json({ success: false, message: "Это время уже недоступно" });
-  }
-
-  const checkSql = `
-  SELECT * FROM bookings 
-  WHERE tableNumber = ? AND date_1 = ? AND fromTime < ? AND toTime > ?
-`;
+// // Пул для подключения к MySQL Railway
+// const pool = mysql.createPool({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   port: process.env.MYSQLPORT,
+//   waitForConnections: true,
+//   connectionLimit: 10
+// });
 
 
-  pool.query(checkSql, [table, date, to, from], (err, results) => {
-    if (err) {
-      console.error("Ошибка проверки занятости:", err);
-      return res.json({ success: false, message: err.message });
-    }
-    if (results.length > 0)
-      return res.json({ success: false, message: "Этот столик уже забронирован" });
+// // Проверка подключения
+// pool.getConnection((err, connection) => {
+//   if (err) {
+//     console.error("Ошибка подключения к MySQL:", err);
+//   } else {
+//     console.log("MySQL подключен!");
+//     connection.release();
+//   }
+// });
 
-    const insertSql = `
-      INSERT INTO bookings (tableNumber, date_1, fromTime, toTime, name_1, surname, phone, comment_1)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+// // API: Получить все брони
+// app.get("/bookings", (req, res) => {
+//   pool.query("SELECT * FROM bookings", (err, results) => {
+//     if (err) {
+//       console.error("Ошибка запроса /bookings:", err);
+//       return res.json([]);
+//     }
+//     res.json(results);
+//   });
+// });
 
-    pool.query(insertSql, [table, date, from, to, name, surname, phone, comment], (err2) => {
-      if (err2) {
-        console.error("Ошибка добавления брони:", err2);
-        return res.json({ success: false, message: err2.message });
-      }
-      res.json({ success: true });
-    });
-  });
-});
+// // API: Создать бронь
+// app.post("/book", (req, res) => {
+//   const { table, date, from, to, name, surname, phone, comment } = req.body;
 
-// -----------------------
-// Запуск сервера
-// -----------------------
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//   const now = new Date();
+//   const bookingDateTime = new Date(`${date}T${from}:00`);
+//   if (bookingDateTime < now) {
+//     return res.json({ success: false, message: "Это время уже недоступно" });
+//   }
+
+//   const checkSql = `
+//   SELECT * FROM bookings 
+//   WHERE tableNumber = ? AND date_1 = ? AND fromTime < ? AND toTime > ?
+// `;
+
+
+//   pool.query(checkSql, [table, date, to, from], (err, results) => {
+//     if (err) {
+//       console.error("Ошибка проверки занятости:", err);
+//       return res.json({ success: false, message: err.message });
+//     }
+//     if (results.length > 0)
+//       return res.json({ success: false, message: "Этот столик уже забронирован" });
+
+//     const insertSql = `
+//       INSERT INTO bookings (tableNumber, date_1, fromTime, toTime, name_1, surname, phone, comment_1)
+//       VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+//     `;
+
+//     pool.query(insertSql, [table, date, from, to, name, surname, phone, comment], (err2) => {
+//       if (err2) {
+//         console.error("Ошибка добавления брони:", err2);
+//         return res.json({ success: false, message: err2.message });
+//       }
+//       res.json({ success: true });
+//     });
+//   });
+// });
+
+// // -----------------------
+// // Запуск сервера
+// // -----------------------
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 // const express = require("express");
--- sql.js
+++ sql.js
@@ -1,26 +1,26 @@
-const mysql = require('mysql2');
-const fs = require('fs');
-require('dotenv').config();
+// const mysql = require('mysql2');
+// const fs = require('fs');
+// require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  multipleStatements: true  // <-- важно для дампа
-});
+// const pool = mysql.createPool({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   multipleStatements: true  // <-- важно для дампа
+// });
 
 
-// Читаем дамп
-const sql = fs.readFileSync('copen.sql', 'utf8');
+// // Читаем дамп
+// const sql = fs.readFileSync('copen.sql', 'utf8');
 
-// Выполняем все команды из дампа
-pool.query(sql, (err, results) => {
-  if (err) console.error(err);
-  else console.log('База успешно загружена на Railway!');
-  pool.end();
-});
-pool.end(() => console.log("Соединение с базой закрыто"));
+// // Выполняем все команды из дампа
+// pool.query(sql, (err, results) => {
+//   if (err) console.error(err);
+//   else console.log('База успешно загружена на Railway!');
+//   pool.end();
+// });
+// pool.end(() => console.log("Соединение с базой закрыто"));
 
